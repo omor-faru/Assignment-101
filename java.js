0e2cfc77f5c26de375d27dfa62da0fcf9765c121
omor github <omorfaruque783@gmail.com>
2025-03-26 03:58:49 +0600

    Assignment101

---
+++ java.js
@@ -0,0 +1,36 @@
+// 1st :  (Ans) Even Or Odd number print  
+
+let num = 24;
+
+
+
+if (num % 2 === 0) {
+    console.log(num, " is even");
+} 
+else {
+    console.log(num,"is odd");
+}
+
+//  2nd: Write a program that calculets grade based score
+
+let score = prompt("(0-100):");
+let grade;
+
+ if (score >= 90 && score <= 100) {
+    grade = "A";
+
+} else if (score >= 80 && score <=89) {
+    grade = "B";
+
+}else if (score >= 70 && score <=79) {
+    grade = "C";
+
+}else if (score >= 60 && score <=69) {
+    grade = "D";
+
+}else if (score >= 0 && score <=60) {
+    grade = "F";
+}
+
+
+console.log("accourding to your scores, your grade was : " , grade ,);
