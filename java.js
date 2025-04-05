8d86273ac99f01e0e130229ea9b83c4997a67182
omor github <omorfaruque783@gmail.com>
2025-04-05 13:08:41 +0600

    media query

--- java.js
+++ java.js
@@ -22,8 +22,6 @@ let grade;
 } else if (score >= 80 && score <=89) {
     grade = "B";
 
-}else if (score >= 70 && score <=79) {
-    grade = "C";
 
 }else if (score >= 60 && score <=69) {
     grade = "D";
